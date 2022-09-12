99e33b39ff63dc512d4868474c19d4d3ff7c5762
DavidBrainy <oppongdavid032@gmail.com>
2022-09-12 14:10:09 +0000

    mongoose-validation

---
+++ modules/posts/post.model.js
@@ -0,0 +1,23 @@
+const {Schema, model} = require ("mongoose");
+
+const postSchema = new Schema({
+    title:{
+        type: String,
+        require:true,
+        minLength: 3,
+    },
+
+    body:{
+        type: String,
+        require:true,
+        minLength: 10,
+    },
+
+    published:{
+        type: Boolean,
+        default: false,
+    },
+
+});
+
+module.exports = model("post", postSchema);
